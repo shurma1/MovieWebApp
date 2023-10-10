c81d70f2d3215c30f92cc2dc02995f832dd7b236
shurma1 <69749761+shurma1@users.noreply.github.com>
2023-10-10 21:03:10 +0300

    Add files via upload

--- backend/src/Handlers/WebAppHandler/WebAppHandler.js
+++ backend/src/Handlers/WebAppHandler/WebAppHandler.js
@@ -8,7 +8,12 @@ const WebAppHandler = async message => {
     const places = data.seats.map((seat) => `${seat.rowMarker}${seat.seat}`).join(', ');
     const total = `$${(data.price*data.seats.length).toFixed(2)} (${data.seats.length}x$${data.price})`;
 
-    const answer = `<b>${data.title} movie tickets are booked</b><b>Your places:</b> ${places}<b>Time:</b> ${data.time}<b>total:</b> ${total}`
+    const answer = `
+<b>${data.title} movie tickets are booked</b>
+<b>Your places:</b> ${places}
+<b>Time:</b> ${data.time}
+<b>total:</b> ${total}
+`
 
     await bot.sendMessage(message.chat.id, answer, {parse_mode: 'HTML'})
 }
